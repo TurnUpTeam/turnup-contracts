4a9756a5a845a073bb459edfe6b006e8fc9047b4
Francesco Sullo <sullof@gmail.com>
2023-11-11 23:00:53 -0800

    Fix post-install

--- post-install.sh
+++ post-install.sh
@@ -10,6 +10,6 @@ if [[ "$EXISTS" == "" ]]; then
     npx mrm@2 lint-staged
     npx husky-init
     pnpm i -D pretty-quick
-    npx husky set .husky/pre-commit "npx pretty-quick --staged && npm run lint && bin/get-coverage.sh"
+    npx husky set .husky/pre-commit "npx pretty-quick --staged && npm run lint && bin/get-coverage.sh && bin/export.sh && git add -A"
 fi
 
