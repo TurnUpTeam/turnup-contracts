3b5c729130db0bb5b650c701acee7b3855f9f63e
qcloud <ubuntu@localhost.localdomain>
2024-01-23 20:48:28 +0800

    add stake event

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ if (process.env.GAS_REPORT === "yes") {
 requirePath(
   ".env",
   `FOR_TESTNET=0xa046b61238d6b3daee76c35b29b4085bd0db4569109c16a7814e86f25ed2aa40 
-FOR_PLOYGON=0xa046b61238d6b3daee76c35b29b4085bd0db4569109c16a7814e86f25ed2aa40 
+FOR_POLYGON=0xa046b61238d6b3daee76c35b29b4085bd0db4569109c16a7814e86f25ed2aa40 
 FEE_DESTINATION=0xe85cfB8C0978bf246C6A923e84616680D12Fd9d7
 `
 );
@@ -47,16 +47,15 @@ module.exports = {
       url: "https://bsc-dataseed.binance.org",
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: [process.env.FOR_PLOYGON],
+      accounts: [process.env.FOR_POLYGON],
     },
     polygon: {
       url: "https://polygon-mainnet.infura.io/v3/a68e6722bae1424687af24044fb1247b",
-      accounts: [process.env.FOR_PLOYGON],
+      accounts: [process.env.FOR_POLYGON],
       chainId: 137,
     },
     mumbai: {
-      // url: "https://rpc-mumbai.maticvigil.com",
-      url: "https://rpc.ankr.com/polygon_mumbai",
+      url: "https://polygon-mumbai.infura.io/v3/a68e6722bae1424687af24044fb1247b",
       accounts: [process.env.FOR_TESTNET],
       chainId: 80001,
     },
