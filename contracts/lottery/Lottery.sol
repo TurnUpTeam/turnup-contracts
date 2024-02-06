89206b7644fea0b29b991d1dfe138edbe0e52b1e
lixg <doingli@foxmail.com>
2024-02-06 14:26:15 +0800

    1

--- contracts/lottery/Lottery.sol
+++ contracts/lottery/Lottery.sol
@@ -259,7 +259,7 @@ contract Lottery is Initializable, OwnableUpgradeable, PausableUpgradeable, Reen
     }
 
     function pickRedPack(uint256 packId) external whenNotPaused nonReentrant {
-        if (!isPickable(pickId, _msgSender())) revert RedPackUnpickable();
+        if (!isPickable(packId, _msgSender())) revert RedPackUnpickable();
         // TODO
         emit PickRedPackRequest(packId, 0, 0, 0, 0, 0, 0);
     }
