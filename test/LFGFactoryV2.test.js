3b91357d935a0d9a4c98279bcc0f73707d346fe0
roquesrolando <roquesrolando@hotmail.com>
2024-02-23 11:36:21 -0400

    added more test

--- test/LFGFactoryV2.test.js
+++ test/LFGFactoryV2.test.js
@@ -117,8 +117,11 @@ describe("LFGFactoryV2", function () {
 
   describe("applyToMintLfg2", function () {
     it("should return the token uri of the contract", async function () {
-      await lfg2.connect(owner).setBaseURI("Uri/");
-      expect(await lfg2.tokenURI(1)).equal("Uri/1");
+      expect(await lfg2.totalSupply()).equal("900000000000000000000000000");
+    });
+
+    it("should return the name of the contract", async function () {
+      expect(await lfg2.name()).equal("LFGTokenV2");
     });
 
     it("should return the name of the contract", async function () {
@@ -172,7 +175,7 @@ describe("LFGFactoryV2", function () {
       expect(await lfg2.balanceOf(bob.address)).equal(amountToSwap);
     });
 
-    it.only("should withdraw for LFG2", async function () {
+    it.skip("should withdraw for LFG2", async function () {
       const orderId = 1;
       const amount = ethers.utils.parseEther("1");
       const amountToSwap = ethers.utils.parseEther("0.5");
@@ -196,6 +199,7 @@ describe("LFGFactoryV2", function () {
       await factory.connect(bob).swapLfgFromV1ToV2(amountToSwap);
       expect(await lfg2.balanceOf(bob.address)).equal(amountToSwap);
       await lfg2.withdraw();
+      console.log(await lfg2.totalSupply());
       console.log(await lfg2.balanceOf(bob.address));
     });
 
