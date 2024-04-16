7371a4f22b9005b354e09e55ae26a11d748c4fd1
lixg <doingli@foxmail.com>
2024-04-16 16:38:10 +0800

    1

--- test/NFTShares.test.js
+++ test/NFTShares.test.js
@@ -3,6 +3,7 @@ const {expect} = require("chai");
 const DeployUtils = require("eth-deploy-utils");
   
 describe("NFTShares", function () {
+  let oneEther;
   let lfg;
   let shares;
   let owner, bob, alice, fred, jim, jane, tokenHolder;
@@ -16,6 +17,8 @@ describe("NFTShares", function () {
   });
 
   async function initAndDeploy() {
+    oneEther = ethers.utils.parseEther("1")
+
     let maxSupply = ethers.utils.parseEther("3000000000");
     let initialSupply = ethers.utils.parseEther("900000000");
     let amountReservedToPool = ethers.utils.parseEther("300000000");
@@ -93,4 +96,50 @@ describe("NFTShares", function () {
     .to.emit(shares, "LFGTokenUpdate")
     .withArgs(bob.address);
   });
+
+  it("should be get protocol fee", async function () {
+    let feePercent = await shares.protocolFeePercent()
+    let one = ethers.utils.parseEther("1");
+    let priceArr = ["0", "0.00", "0.01", "0.9888", "1", "1.5", "1.999", "1200", "100080001", "999999999"]
+    for (let p in priceArr) {
+      let price = ethers.utils.parseEther(p);
+      expect(await shares.getProtocolFee(price)).to.equal(price.mul(feePercent).div(one));
+    }
+  });
+
+  it("should be get subject fee", async function () {
+    let feePercent = await shares.subjectFeePercent()
+    let one = ethers.utils.parseEther("1");
+    let priceArr = ["0", "0.00", "0.01", "0.9888", "1", "1.5", "1.999", "1200", "100080001", "999999999"]
+    for (let p in priceArr) {
+      let price = ethers.utils.parseEther(p);
+      expect(await shares.getSubjectFee(price)).to.equal(price.mul(feePercent).div(one));
+    }
+  });
+
+  it("should be get price", async function () {
+    for (let i = 0; i < 10000; i += 33) {
+      expect(await shares.getPrice(i, 1)).to.equal(oneEther.mul((i + 1) * (i + 1) * 5));
+    }
+    let supplyList = [0, 1, 3, 7, 13, 32, 57, 60, 82, 100, 160, 200, 230, 190, 330, 550, 700, 870, 960, 1060, 2030, 5000];
+    let amountList = [1, 3, 6, 9, 12, 19, 26, 37, 49, 80, 99, 100];
+    for (let i = 0; i < supplyList.length; i++) {
+      for (let j = 0; j < amountList.length; j++) {
+        let s = supplyList[i];
+        let a = amountList[j];
+        let expectSum = ethers.utils.parseEther("0");
+        for (let k = 0; k < a; k++) {
+          let p = oneEther.mul((s + 1 + k) * (s + 1 + k) * 5)
+          expectSum = expectSum.add(p)
+        }
+        let actualSum = await shares.getPrice(s, a);
+        expect(actualSum).to.equal(expectSum);
+      }
+    }
+  });
+
+  it("should be get buy price for supply 0", async function () {
+
+  });
+
 });
