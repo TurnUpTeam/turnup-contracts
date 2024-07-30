5abe31bbf4bf740d34eb71729afba9589e04751a
doingli <doingli@foxmail.com>
2024-07-30 11:27:43 +0800

    1

---
+++ deploy/deploy-abstract-moments.js
@@ -0,0 +1,39 @@
+const { utils, Wallet } = require('zksync-ethers')
+const { Deployer } = require('@matterlabs/hardhat-zksync')
+const { HardhatRuntimeEnvironment } = require('hardhat/types')
+// const* as ethers from "ethers";
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+
+module.exports = async function (hre) {
+  const {
+    FIRST_VALIDATOR,
+    ABSTRACT_TESTNET_WETH,
+  } = process.env
+
+  console.log(`validator ${FIRST_VALIDATOR}`) 
+  console.log(`abstract testnet weth ${ABSTRACT_TESTNET_WETH}`)
+
+  let wallet = await hre.zksyncEthers.getWallet(0)
+  let deployer = new Deployer(hre, wallet)
+  
+  let contract = await deployer.loadArtifact('MomentFactory')
+  let factory = await hre.zkUpgrades.deployProxy(wallet, contract, [[FIRST_VALIDATOR,], ABSTRACT_TESTNET_WETH])
+  console.log(`Moment factory deployed tx at: ${factory.deployTransaction.hash}`)
+  await factory.waitForDeployment()
+  let factoryAddr = await factory.getAddress()
+  console.log('Moment factory deployed to:', factoryAddr)
+
+  contract = await deployer.loadArtifact('MomentToken')
+  let tokens = await hre.zkUpgrades.deployProxy(wallet, contract, [factoryAddr])
+  console.log(`Moment Token deployed tx at: ${tokens.deployTransaction.hash}`)
+  await tokens.waitForDeployment()
+  let tokensAddr = await tokens.getAddress()
+  console.log('Moment Token deployed to:', tokensAddr)
+
+  factory.connect(wallet)
+  console.log(factory)
+  let tx = await factory.setMomentToken(tokensAddr)
+  console.log(tx)
+}
+
